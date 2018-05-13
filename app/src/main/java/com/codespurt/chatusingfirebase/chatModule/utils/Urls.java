551518fe9be44acda152f354cb216100111f8d4b
Code Spurt <codespurt@gmail.com>
2018-05-13 23:16:26 +0530

    Initial Commit

--- app/src/main/java/com/codespurt/chatusingfirebase/chatModule/utils/Urls.java
+++ app/src/main/java/com/codespurt/chatusingfirebase/chatModule/utils/Urls.java
@@ -6,7 +6,7 @@ package com.codespurt.chatusingfirebase.chatModule.utils;
 
 public class Urls {
 
-    public static final String FIREBASE_DATABASE_URL = "https://demos-6b08b.firebaseio.com/";
+    public static final String FIREBASE_DATABASE_URL = "";
 
     public static final String FIREBASE_TABLE_NAME_USERS = "users.json";
     public static final String FIREBASE_TABLE_NAME_MESSAGES = "messages/";
